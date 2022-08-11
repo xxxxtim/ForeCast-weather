a355f912dfa77ea49fe19db57b6887e3fbf5abaa
yenting <yenting@yentingdeMacBook-Pro.local>
2022-08-12 00:34:28 +0800

    feat: fetch cityname api and get return success

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOption = {
+  headers: {
+    "X-RapidAPI-Key": "948ce3ac06msh4de7737d08e2d03p1a1632jsne916572a94fe",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
