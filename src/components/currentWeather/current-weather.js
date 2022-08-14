4bf6f5929b2b39bd35abd0e6fa1c53903d46dd8a
yenting <yenting@yentingdeMacBook-Pro.local>
2022-08-15 00:02:49 +0800

    feat: weather card css completed

--- src/components/currentWeather/current-weather.js
+++ src/components/currentWeather/current-weather.js
@@ -3,10 +3,36 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">tokyo</p>
-        <p className="weather-dscrb">sunny</p>
+        <div>
+          <p className="city">tokyo</p>
+          <p className="weather-dscrb">sunny</p>
+        </div>
+        <img src="icon/01d.png" alt="weather" className="weather-icon" />
       </div>
-      <img src="" alt="weather" className="weather-icon" />
+      <div className="bottom">
+        <p className="temperature"> 34°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="paremeter-label">details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="paremeter-label">feels like</span>
+            <span className="paremeter-value">18°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="paremeter-label">wind</span>
+            <span className="paremeter-value">30 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="paremeter-label">Humidity</span>
+            <span className="paremeter-value">20 %</span>
+          </div>
+          <div className="parameter-row">
+            <span className="paremeter-label">Pressure</span>
+            <span className="paremeter-value">15 hpa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
